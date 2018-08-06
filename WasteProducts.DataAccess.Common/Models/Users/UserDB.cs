a27bcd6e84188240e5445639badd64a284f571f7
Alexander Yashyn <yashyn.alexander@gmail.com>
2018-08-06 23:08:52 +0300

    Model UserDB is created with properties.

---
+++ WasteProducts.DataAccess.Common/Models/Users/UserDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasteProducts.DataAccess.Common.Models.Users
+{
+    internal class UserDB
+    {
+
+        public int UserId { get; }
+
+        public string UserName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreatedOn { get; }
+    }
+}
