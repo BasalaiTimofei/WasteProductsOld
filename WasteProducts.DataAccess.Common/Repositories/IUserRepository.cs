c8802f654f42c00a9583c421ed9f3d3977f645b7
SergeiTishkov <tishkovsergei92@gmail.com>
2018-08-07 18:10:07 +0300

    IUserService added and populated, blank User model added

--- WasteProducts.DataAccess.Common/Repositories/IUserRepository.cs
+++ WasteProducts.DataAccess.Common/Repositories/IUserRepository.cs
@@ -2,6 +2,5 @@
 {
     interface IUserRepository
     {
-        // TODO Привет Саня
     }
 }
---
+++ WasteProducts.Logic.Common/Services/IUserService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WasteProducts.Logic.Common.Models.Users;
+
+namespace WasteProducts.Logic.Common.Services
+{
+    public interface IUserService
+    {
+        bool Register(string email, string password, string passwordConfirmation, out User registeredUser);
+
+        bool LogIn(string email, string password, out User loggedInUser);
+
+        bool ResetPassword(User user, string oldPassword, string newPassword, string newPasswordConfirmation);
+
+        void ResetPasswordQuery(string email);
+
+        bool ResetPasswordResponse(string newPassword, string newPasswordConfirmation);
+
+        bool SetUserName(User user, string userName);
+
+        void AddFriend(User newFriend);
+
+        void DeleteFriend(User deletingFriend);
+
+        // TODO USER MANAGEMENT PENDING FUNCTIONAL TO ADD:
+        // sharing my products with my friends after model "Product" is created
+        // subscribing special users to watch their news (if this functional will be approved)
+        // chatting between users
+        // registering by Facebook and VK profiles
+        // getting "Approved Representative of The Company" status and its unique functional like special tools for speed feedback
+
+    }
+}
