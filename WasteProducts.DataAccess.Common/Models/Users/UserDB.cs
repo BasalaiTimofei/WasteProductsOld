472a4a22d0d0b61b796818101ab6cbc32e16046a
Alexander Yashyn <yashyn.alexander@gmail.com>
2018-08-08 20:25:34 +0300

    Removed unnecessary Using.

--- WasteProducts.DataAccess.Common/Models/Users/UserDB.cs
+++ WasteProducts.DataAccess.Common/Models/Users/UserDB.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WasteProducts.DataAccess.Common.Enums;
 
 namespace WasteProducts.DataAccess.Common.Models.Users
 {
