a39a1f0fc61a15a414247665143b25695be9ee9c
Alexander Yashyn <yashyn.alexander@gmail.com>
2018-08-08 20:23:29 +0300

    Enum is moved to BLL. Properties of User and UserDB models are documented. Enum is documented.

--- WasteProducts.DataAccess.Common/Models/Users/UserDB.cs
+++ WasteProducts.DataAccess.Common/Models/Users/UserDB.cs
@@ -9,38 +9,69 @@ namespace WasteProducts.DataAccess.Common.Models.Users
 {
     public class UserDB
     {
-        public int Id { get; }
-
-        public string Login { get; }
-
-        public string Email { get; }
-
-        public string Password { get; }
-
-        public List<UserDB> UserFriends { get; }
-
-        // public List<Product> UserProducts { get; }
-
-        public List<int> GroupMembership { get; }
-
-        public UserCategory UserTraits { get; }
+        /// <summary>
+        /// Unique identifier of concrete User in Database.
+        /// </summary>
+        public int Id { get; set; }
         
-        public bool IsEnabled { get; } = true;
+        /// <summary>
+        /// Unique name of concrete User. It is used for authenfication.
+        /// </summary>
+        public string Login { get; set; }
 
-        public int? ImageId { get; }
+        /// <summary>
+        /// Email of User is mandatory property and it is set during registration. It is used for password recovery.
+        /// </summary>
+        public string Email { get; set; }
 
-        public int? Priviledge { get; }
+        /// <summary>
+        /// Password is set by User during registration.
+        /// </summary>
+        public string Password { get; set; }
 
-        public DateTime CreatedOn { get; }
+        /// <summary>
+        /// List of Users which belong to group of friends related to current User.
+        /// </summary>
+        public List<UserDB> UserFriends { get; set; }
 
-        public DateTime? LastEditedOn { get; }
+        /// <summary>
+        /// List of Products which User have ever captured.
+        /// </summary>
+        //public List<Product> UserProducts { get; set; }
 
-        public UserDB(string login, string email, string password)
-        {
-            Login = login;
-            Email = email;
-            Password = password;
-            CreatedOn = DateTime.Now;
-        }
+        /// <summary>
+        /// List of all Groups to which current User is assigned.
+        /// </summary>
+        //public List<Group> GroupMembership { get; set; }
+
+        /// <summary>
+        /// Defines User's belonging to some specific Group of customers. E.g. User can be diabetics and prefere low carb Products. Or sportsmen and prefere high protein diet.
+        /// </summary>
+        public int UserTraits { get; set; }
+        
+        /// <summary>
+        /// Defines if User is banned by Admin or not. True refers to not banned. False refers to banned.
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Identifier of image that is avatar of User. Can be NULL - without avatar.
+        /// </summary>
+        public int? ImageId { get; set; }
+
+        /// <summary>
+        /// Privildge which assigned to the User. Can be User or Admin.
+        /// </summary>
+        public int Priviledge { get; set; }
+
+        /// <summary>
+        /// Specifies timestamp of creation of concrete User in Database.
+        /// </summary>
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Specifies timestamp of modifying of any Property of User in Database.
+        /// </summary>
+        public DateTime? LastEditedOn { get; set; }
     }
 }
--- WasteProducts.Logic.Common/Models/Users/User.cs
+++ WasteProducts.Logic.Common/Models/Users/User.cs
@@ -1,40 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using WasteProducts.Logic.Common.Enums;
 
 namespace WasteProducts.Logic.Common.Models.Users
 {
     public class User
     {
-        public int Id { get; }
+        /// <summary>
+        /// Unique identifier of concrete User in Database.
+        /// </summary>
+        public int Id { get; set; }
 
-        public string Login { get; }
+        /// <summary>
+        /// Unique name of concrete User. It is used for authenfication.
+        /// </summary>
+        public string Login { get; set; }
 
-        public string Email { get; }
+        /// <summary>
+        /// Email of User is mandatory property and it is set during registration. It is used for password recovery.
+        /// </summary>
+        public string Email { get; set; }
 
-        public string Password { get; }
+        /// <summary>
+        /// Password is set by User during registration.
+        /// </summary>
+        public string Password { get; set; }
 
-        public List<User> UserFriends { get; }
+        /// <summary>
+        /// List of Users which belong to group of friends related to current User.
+        /// </summary>
+        public List<User> UserFriends { get; set; }
 
-        // public List<Product> UserProducts { get; }
+        /// <summary>
+        /// List of Products which User have ever captured.
+        /// </summary>
+        //public List<Product> UserProducts { get; set; }
 
-        public List<int> GroupMembership { get; }
+        /// <summary>
+        /// List of all Groups to which current User is assigned.
+        /// </summary>
+        //public List<Group> GroupMembership { get; set; }
 
-        // [TBD] public UserCategory UserTraits { get; }
+        /// <summary>
+        /// Defines User's belonging to some specific Group of customers. E.g. User can be diabetic and prefere low carb Products. Sportsmen prefere high protein diet.
+        /// </summary>
+        public UserCategory UserTraits { get; set; }
 
-        public bool IsEnabled { get; } = true;
+        /// <summary>
+        /// Defines if User is banned by Admin or not. True refers to not banned. False refers to banned.
+        /// </summary>
+        public bool IsEnabled { get; set; }
 
-        public int? ImageId { get; }
+        /// <summary>
+        /// Identifier of image that is avatar of User. Can be NULL - without avatar.
+        /// </summary>
+        public int? ImageId { get; set; }
 
-        public int? Priviledge { get; }
-
-        public User(string login, string email, string password)
-        {
-            Login = login;
-            Email = email;
-            Password = password;
-        }
+        /// <summary>
+        /// Privildge which assigned to the User. Can be User or Admin.
+        /// </summary>
+        public int Priviledge { get; set; }
     }
 }
