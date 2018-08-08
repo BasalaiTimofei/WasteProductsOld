5369ebafd233d165ff65c33d4d81849219df7e1d
Kritsky Alexander <kritckiy.alexader@gmail.com>
2018-08-08 21:27:21 +0300

    Add SearchQuery model
    Added model, that used to create search query and includes search fields
    Resolved: #105

---
+++ WasteProducts.Logic.Common/Models/Search/SearchQuery.cs
@@ -0,0 +1,23 @@
+﻿namespace WasteProducts.Logic.Common.Models
+{
+    /// <summary>
+    /// Model uses for search
+    /// </summary>
+    class SearchQuery
+    {
+        /// <summary>
+        /// The query that contains text to search
+        /// </summary>
+        string Query { get; set; }
+
+        /// <summary>
+        /// The array of fields (model properties) of search
+        /// </summary>
+        string[] SearchableFields { get; set; }
+
+        //Александр,
+        //нормально ли будет заменить на такой вариант?
+        //Массиы типов моделей. Далее через рефлексию найти методы вида Search и через них осуществлять поиск по всем доступным полям     
+        //Type[] _searchableTypesFields;
+    }
+}
