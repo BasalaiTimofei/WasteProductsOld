55fb5d60d84883312dfd9f2e94d5fc6d70d08858
Dmitry Gapeev <exelero86@gmail.com>
2018-08-08 16:22:06 +0300

    Added Product and ProductDB models

---
+++ WasteProducts.DataAccess.Common/Models/Product/ProductDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasteProducts.DataAccess.Common.Models.Product
+{
+    public class ProductDB
+    {
+        public string Name { get; set; }
+        public string Id { get; }
+        public DateTime Created { get; }
+        public DateTime Updated { get; }
+        public Category Category { get; set; }
+        public Barcode Barcode { get; set; }
+        public double AvgMark { get; internal set; }
+        public decimal Price { get; }
+        public int RateCount { get; internal set; }
+    }
+}
---
+++ WasteProducts.Logic.Common/Models/Product/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasteProducts.Logic.Common.Models.Product
+{
+    public enum Category
+    {
+
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public Barcode Barcode { get; set; }
+        public double AvgMark { get; internal set; }
+        public decimal Price { get; set; }
+
+        //для вычисления средней оценки нам понадобится количество оценивших данный продукт
+        public int RateCount { get; internal set; } 
+    }
+}
