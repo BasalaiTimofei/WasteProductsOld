6d8a9d68223310dbb9f8181180cde1d73d82cac2
tpitlik <taras.pitlik@gmail.com>
2018-08-09 15:16:07 +0300

    Added comments.

---
+++ WasteProducts.DataAccess.Common/Repositories/Search/ISearchRepository.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WasteProducts.DataAccess.Common.Repositories.Search
+{
+    /// <summary>
+    /// This interface provides CRUD methods for search repository
+    /// </summary>
+    public interface ISearchRepository
+    {
+        /// <summary>
+        /// Returns object by Id
+        /// </summary>
+        /// <typeparam name="TEntity">Object model type</typeparam>
+        /// <param name="Id">Id of model</param>
+        /// <returns>Object model</returns>
+        TEntity Get<TEntity>(string Id);
+        
+        /// <summary>
+        /// Async version of Get
+        /// </summary>
+        Task<TEntity> GetAsync<TEntity>(string Id);
+
+        /// <summary>
+        /// Returnes collection of all objects
+        /// </summary>
+        /// <typeparam name="TEntity">Object model type</typeparam>
+        /// <returns>IEnumerable of objects</returns>        
+        IEnumerable<TEntity> GetAll<TEntity>();
+
+        /// <summary>
+        /// Async version of GetAll
+        /// </summary>
+        Task<IEnumerable<TEntity>> GetAllAsync<TEntity>();
+
+        /// <summary>
+        /// Insert object to lucene object storage
+        /// </summary>
+        /// <typeparam name="TEntity">Object model type</typeparam>
+        /// <param name="obj">Object</param>
+        void Insert<TEntity>(TEntity obj);
+
+        /// <summary>
+        /// Async version of Insert
+        /// </summary>
+        Task InsertAsync<TEntity>(TEntity obj);
+
+        /// <summary>
+        /// Update object in lucene object storage
+        /// </summary>
+        /// <typeparam name="TEntity">Object model type</typeparam>
+        /// <param name="obj">Object</param>
+        void Update<TEntity>(TEntity obj);
+
+        /// <summary>
+        /// Async version of Update
+        /// </summary>
+        Task UpdateAsync<TEntity>(TEntity obj);
+
+        /// <summary>
+        /// Delete object from lucene object storage
+        /// </summary>
+        /// <typeparam name="TEntity">Object model type</typeparam>
+        /// <param name="obj">Object</param>
+        void Delete<TEntity>(TEntity obj);
+
+        /// <summary>
+        /// Async version of Delete
+        /// </summary>
+        Task DeleteAsync<TEntity>(TEntity obj);        
+    }
+}
