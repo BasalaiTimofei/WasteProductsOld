85eb23dd9fa5a59a704d2e77e41b615be152fe04
Kritsky Alexander <kritckiy.alexader@gmail.com>
2018-08-09 05:22:25 +0300

    Add ISearchService that provides search methods
    Added a service with search methods

---
+++ WasteProducts.Logic.Common/Services/Search/ISearchService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WasteProducts.Logic.Common.Models;
+
+namespace WasteProducts.Logic.Common.Services.Search
+{
+    /// <summary>
+    /// This interface provides search methods
+    /// </summary>
+    public interface ISearchService
+    {
+        /// <summary>
+        /// This method provides ability to search by partial words. It replaces all dashes "-" in search requests, and adds "*" (star) after each word.
+        /// </summary>
+        /// <typeparam name="TEntity">Object model</typeparam>
+        /// <param name="query">SearchQuery model</param>
+        /// <returns>IEnumerable of data</returns>
+        IEnumerable<TEntity> Search<TEntity>(SearchQuery query);
+        Task<IEnumerable<TEntity>> SearchAsync<TEntity>(SearchQuery query);
+
+        /// <summary>
+        /// This method provides ability to search without any formatting at search requests
+        /// </summary>
+        /// <typeparam name="TEntity">Object model</typeparam>
+        /// <param name="query">SearchQuery model</param>
+        /// <returns>IEnumerable of data</returns>
+        IEnumerable<TEntity> SearchDefault<TEntity>(SearchQuery query);
+        Task<IEnumerable<TEntity>> SearchDefaultAsync<TEntity>(SearchQuery query);
+
+        //Александр,
+        //Предполагалось, что данные методы будут создавать\обновлять\удалять индекс в Lucene.
+        //Правльно ли я понял, что здесь они не нужны и их можно будет создать когда начнем конфигурировать context?
+
+        //добвление\обновление индекса при первом запуске, а также при добавлении нового объкта в базу
+        //void AddUpdateIndex<TEntity>(IEnumerable<TEntity> model);
+        //void AddUpdateIndex<TEntity>(TEntity model);
+        //удаление индекса целиком и при удалении объекта
+        //void ClearIndexRecord<TEntity>(TEntity model);
+        //bool ClearIndex();
+    }
+}
