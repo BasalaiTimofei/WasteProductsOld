a28c953ad9ad9bdb3a4a7f024344e9ad5f9b661b
Kritsky Alexander <kritckiy.alexader@gmail.com>
2018-08-09 05:37:52 +0300

    Add ISearchRepository interface

---
+++ WasteProducts.DataAccess.Common/Repositories/Search/ISearchRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WasteProducts.DataAccess.Common.Repositories.Search
+{
+    /// <summary>
+    /// This interface provides SearchRepository methods
+    /// </summary>
+    public interface ISearchRepository
+    { 
+        /// <summary>
+        /// Returnes object by Id
+        /// </summary>
+        /// <typeparam name="TEntity">Object model</typeparam>
+        /// <param name="Id">Id of model</param>
+        /// <returns>Object model</returns>
+        TEntity Get<TEntity>(string Id);
+        Task<TEntity> GetAsync<TEntity>(string Id);
+
+        /// <summary>
+        /// Returnes collection of all objects
+        /// </summary>
+        /// <typeparam name="TEntity">Object model</typeparam>
+        /// <returns>IEnumerable of objects</returns>        
+        IEnumerable<TEntity> GetAll<TEntity>();
+        Task<IEnumerable<TEntity>> GetAllAsync<TEntity>();
+    }
+}
