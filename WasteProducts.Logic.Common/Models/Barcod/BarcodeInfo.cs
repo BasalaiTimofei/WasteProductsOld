a3065e6b0cf3ed4af8ad88acb65574d2c290c987
Ilya Sauko <garsavko@mail.ru>
2018-08-10 00:19:45 +0300

    Added poor model and interface barcode service. Added links to System.Drawing in WasteProducts.Logic.Common.
    Signed-off-by: Ilya Sauko <garsavko@mail.ru>

---
+++ WasteProducts.Logic.Common/Models/Barcod/BarcodeInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodServise
+{
+    public class BarcodeInfo
+    {
+        /// <summary>
+        /// Barcode number.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Product name.
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Product brend.
+        /// </summary>
+        public string Brend { get; set; }
+
+        /// <summary>
+        /// Product country.
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Barcode Type.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Product weight.
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// User ID of the sending photo.
+        /// </summary>
+        public int UserID { get; set; }
+    }
+}
---
+++ WasteProducts.Logic.Common/Services/IBarcodService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodServise
+{
+    public interface IBarcodService
+    {
+        /// <summary>
+        /// get a numerical barcode on the photo
+        /// </summary>
+        /// <param name="image">image - barcode photo</param>
+        /// <returns>a numerical barcode</returns>
+        string GetCode(Image image);
+
+        /// <summary>
+        /// get product information
+        /// </summary>
+        /// <param name="code">a numerical barcode</param>
+        /// <returns>product information in the format BarcodeInfo</returns>
+        BarcodeInfo GetInfo(string code);
+    }
+}
