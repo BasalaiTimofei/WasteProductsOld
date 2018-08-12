b2484a1710b786a274e2019da24db8dbaeec4939
Taras Pitlik <taras.pitlik@gmail.com>
2018-08-12 12:57:52 +0300

    Added implementation of LuceneSearchRepository and LuceneSearchService with stubs.

---
+++ WasteProducts.DataAccess/Repositories/LuceneSearchRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WasteProducts.DataAccess.Common.Repositories.Search;
+
+namespace WasteProducts.DataAccess.Repositories
+{
+    /// <summary>
+    /// Implementation of ISearchRepository with Lucene
+    /// </summary>
+    public class LuceneSearchRepository : ISearchRepository
+    {
+        public TEntity Get<TEntity>(string keyValue, string keyField = "Id")
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll<TEntity>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert<TEntity>(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<TEntity>(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<TEntity>(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        //todo: delete async methods later
+  
+        public Task<TEntity> GetAsync<TEntity>(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync<TEntity>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync<TEntity>(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync<TEntity>(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync<TEntity>(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WasteProducts.Logic/Services/LuceneSearchService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WasteProducts.DataAccess.Common.Repositories.Search;
+using WasteProducts.Logic.Common.Models;
+using WasteProducts.Logic.Common.Models.Search;
+using WasteProducts.Logic.Common.Services.Search;
+
+namespace WasteProducts.Logic.Services
+{
+    /// <summary>
+    /// Implementation of ISearchService with Lucene
+    /// </summary>
+    public class LuceneSearchService : ISearchService
+    {
+        private ISearchRepository _repository;
+        public LuceneSearchService(ISearchRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public SearchResult Search<TEntity>(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SearchResult SearchAll(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SearchResult SearchDefault<TEntity>(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SearchResult SearchAllDefault(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddToSearchIndex<TEntity>(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddToSearchIndex<TEntity>(IEnumerable<TEntity> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromSearchIndex<TEntity>(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveFromSearchIndex<TEntity>(IEnumerable<TEntity> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateInSearchIndex<TEntity>(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateInSearchIndex<TEntity>(IEnumerable<TEntity> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ClearSearchIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OptimizeSearchIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        //todo: implement async methods later
+
+        public Task<SearchResult> SearchAsync<TEntity>(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SearchResult> SearchAllAsync(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SearchResult> SearchDefaultAsync<TEntity>(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SearchResult> SearchAllDefaultAsync(SearchQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
