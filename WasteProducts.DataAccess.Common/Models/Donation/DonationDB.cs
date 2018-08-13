ee0f0b3176597c1a48f85cbe3e1c62b1c818e0bd
Andrew Palcheuski <timplayer@tut.by>
2018-08-13 16:36:59 +0300

    Add DonationDB class.

---
+++ WasteProducts.DataAccess.Common/Models/Donation/DonationDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WasteProducts.DataAccess.Common.Models.Donation
+{
+    class DonationDB
+    {
+        /// <summary>
+        /// Unique identifier for a specific donation.
+        /// </summary>
+        [Key]
+        public int PaymentNo { get; set; }
+
+        /// <summary>
+        /// Specifies the date of donation.
+        /// </summary>
+        public DateTime PaymentDate { get; set; }
+
+        /// <summary>
+        /// Specifies the amount of donation.
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Specifies the currency code.
+        /// </summary>
+        public int Currency { get; set; }
+
+        /// <summary>
+        /// Specifies the timestamp for creating of a specific donation in the database.
+        /// </summary>
+        public DateTime CreatedOn { get; set; }
+    }
+}
