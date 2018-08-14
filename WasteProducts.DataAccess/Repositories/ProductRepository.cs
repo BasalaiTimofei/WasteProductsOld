8e0344b845b55fdbdf3b499c1291d59c316debb4
aleks <bad.bad.jaguar@gmail.com>
2018-08-15 00:41:31 +0300

    Based on the comments removed all the tags '<ItemGroup />'. also, some amendments have been made.

--- WasteProducts.DataAccess/Repositories/ProductRepository.cs
+++ WasteProducts.DataAccess/Repositories/ProductRepository.cs
@@ -53,7 +53,7 @@ namespace WasteProducts.DataAccess.Repositories
         /// The method that delets the product by ID.
         /// </summary>
         /// <param name="id">Product's ID that needs to be deleted.</param>
-        public void DeleteById(string id)
+        public void DeleteById(int id)
         {
             var product = _context.Products.Find(id);
             if (product != null) _context.Products.Remove(product);
@@ -92,7 +92,7 @@ namespace WasteProducts.DataAccess.Repositories
         /// </summary>
         /// <param name="id">The specific id of product that was sorted.</param>
         /// <returns>Product by ID.</returns>
-        public ProductDB GetById(string id) => _context.Products.Find(id);
+        public ProductDB GetById(int id) => _context.Products.Find(id);
 
         /// <summary>
         /// This method allows you to modify some or all of the product values.
