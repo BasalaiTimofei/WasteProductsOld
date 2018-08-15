63aa456f6e0f37165320ba82b100cc93754f3fe1
tpitlik <taras.pitlik@gmail.com>
2018-08-15 17:13:49 +0300

    Added test for LuceneSearchRepository.

---
+++ WasteProducts.Logic.Tests/Search_Tests/LuceneSearchRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using WasteProducts.DataAccess.Repositories;
+using WasteProducts.Logic.Common.Models.Users;
+using FluentAssertions;
+
+namespace WasteProducts.Logic.Tests.Search_Tests
+{
+    [TestFixture]
+    public class LuceneSearchRepositoryTest
+    {
+
+        private IEnumerable<User> users;
+        LuceneSearchRepository sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new LuceneSearchRepository();
+            sut.Clear();
+        }
+
+        [Test]
+        public void TestGetFromRepository()
+        {
+            sut.Clear();
+            var user1 = new User { Id = 1, Login = "user1", Email = "user1@mail.net" };
+            var user2 = new User { Id = 2, Login = "user2", Email = "user2@mail.net" };
+            sut.Insert<User>(user1);
+            sut.Insert<User>(user2);
+            var resultUser1 = sut.Get<User>("user1", "Login");
+            var resultUser2 = sut.Get<User>("user2", "Login");
+            resultUser1.Should().BeEquivalentTo(user1);
+            resultUser2.Should().BeEquivalentTo(user2);
+        }
+    }
+}
