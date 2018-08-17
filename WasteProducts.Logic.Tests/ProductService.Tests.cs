2c25040cb209bc1a9127431de42481046a2f7437
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2018-08-17 18:09:17 +0300

    Adding NSubtitute to tests proj.

--- WasteProducts.Logic.Tests/ProductService.Tests.cs
+++ WasteProducts.Logic.Tests/ProductService.Tests.cs
@@ -2,17 +2,31 @@
 using System.ComponentModel;
 using Moq;
 using NUnit.Framework;
+using NSubstitute;
+using WasteProducts.DataAccess.Common.Repositories;
 
 namespace WasteProducts.Logic.Tests
 {
     [TestFixture]
-    public class ProductService
+    public class ProductServiceTests
     {
+        class BarcodeInfo
+        {
+            private int barcode = 654298;
+
+            public int GetInfo(int barcode)
+            {
+                return barcode;
+            }
+        }
         [Test]
         public void AddingProductByBarcore()
         {
             bool productAdded;
-            var barcodeMock = new Mock();
+            int barcode = 654298;
+
+            var product = Substitute.For<IProductRepository>();
+            Assert.AreEqual(product.AddBy));
         }
     }
 }
