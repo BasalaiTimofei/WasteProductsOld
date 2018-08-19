f3a34de0e25a3924275d31738dcbcee8ccc5f553
aleks <bad.bad.jaguar@gmail.com>
2018-08-19 16:24:29 +0300

    Added FluentAssertions for TDD and description for it. Added some unit tests.

--- WasteProducts.Logic.Tests/ProductService.Tests.cs
+++ WasteProducts.Logic.Tests/ProductService.Tests.cs
@@ -3,32 +3,44 @@ using System.ComponentModel;
 using Moq;
 using NUnit.Framework;
 using NSubstitute;
+using FluentAssertions;
 using WasteProducts.Logic.Common.Models.Barcods;
 using WasteProducts.Logic.Common.Models.Products;
 using WasteProducts.Logic.Common.Services;
 
 namespace WasteProducts.Logic.Tests
 {
+    #region NSubstitute description
     //What is NSubstitute? http://nsubstitute.github.io/
     //NSubstitute is designed as a friendly substitute for .NET mocking libraries.
     //It is an attempt to satisfy our craving for a mocking library with a succinct syntax that helps 
     //us keep the focus on the intention of our tests, rather than on the configuration of our test doubles.
     //We've tried to make the most frequently required operations obvious and easy to use, keeping less usual
     //scenarios discoverable and accessible, and all the while maintaining as much natural language as possible.
+    #endregion
+
+    #region FluentAssertions descreiption
+//https://fluentassertions.com/documentation/
+//As you may have noticed, the purpose of this open-source project is to not only be the best 
+//assertion framework in the.NET realm, but to also demonstrate high-quality code. We heavily 
+//practice Test Driven Development and one of the promises TDD makes is that unit tests can be
+//treated as your API’s documentation. So although you are free to go through the many examples
+//here, please consider to analyze the many unit tests.
+    #endregion
 
     [TestFixture]
     public class ProductServiceTests
     {
-        private IProductService _product;
-        private IBarcodeService _barcode;
+        private IProductService _productSrvc;
+        private Barcode _barcode;
         private bool _added;
         private bool _notAdded;
 
         [SetUp]
         public void Init()
         {
-            _product = Substitute.For<IProductService>();
-            _barcode = Substitute.For<IBarcodeService>();
+            _barcode = new Barcode { Code = "125478569", Brand = "Mars", Country = "Russia", Id = "51515" };
+            _productSrvc = Substitute.For<IProductService>();
             _added = true;
             _notAdded = false;
         }
@@ -36,33 +48,58 @@ namespace WasteProducts.Logic.Tests
         [Test]
         public void AddingProductByBarcore_Added()
         {
-            var barcode = new Barcode
-            {
-                Code = "125478569",
-            };
-            var isSuccess = _product.AddByBarcode(barcode);
-            isSuccess.Returns(_added);
-
+            var isSuccess = _productSrvc.AddByBarcode(_barcode);
+            
             Assert.That(isSuccess);
-            Assert.That(!isSuccess);
         }
+
+        [Test]
+        public void AddingProductByBarcore_IfBarcodesAreEqualShouldNotBeAdded()
+        {
+            var barcode1 = new Barcode();
+            var barcode2 = new Barcode();
+            
+            var isSuccess = _productSrvc.AddByBarcode(barcode1);
+
+            barcode1.Should().BeSameAs(barcode2);
+            isSuccess.Should().BeFalse();
+        }
+
+        [Test]
+        public void AddingProductByBarcore_BarcodeIsNotNull()
+        {
+            _barcode.Should().NotBe(null);
+            var isSuccess = _productSrvc.AddByBarcode(_barcode);
+
+            isSuccess.Should().BeTrue();
+        }
+
         [Test]
         public void AddingProductByName_SuccessfullyAddedProduct()
         {
-            var name = "Chocolate";
-            _product.AddByName(name).Returns(_added);
+            var name = "Choco";
 
-            Assert.AreEqual(name, _added);
+            var result = _productSrvc.AddByName(name);
+
+            name.Should().NotBe(null);
+            result.Should().Be(_added);
         }
 
         [Test]
         public void AddingProductByName_WasNotAddedProduct()
         {
-            var name = "Chocolate";
-            var goal = _product.AddByName(name);
-            goal.Returns(_notAdded);
+            var chocolate = "Chocolate";
+            var goal = _productSrvc.AddByName(chocolate);
 
-            Assert.IsFalse(goal);
+            goal.Should().BeFalse();
+        }
+
+        [Test]
+        public void AddingProductByName_NameIsNull()
+        {
+            var goal = _productSrvc.AddByName(null);
+
+            Assert.IsTrue(goal);
         }
     }
 }
