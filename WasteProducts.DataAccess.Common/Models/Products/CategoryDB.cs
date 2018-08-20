f199552a1100675cdf54d705716bcc168424f7e0
Dmitry Gapeev <exelero86@gmail.com>
2018-08-20 23:17:43 +0300

    Small fix in Category models

--- WasteProducts.DataAccess.Common/Models/Products/CategoryDB.cs
+++ WasteProducts.DataAccess.Common/Models/Products/CategoryDB.cs
@@ -25,6 +25,6 @@ namespace WasteProducts.DataAccess.Common.Models.Products
         /// <summary>
         /// List of products that belong to a specific Сategory in database
         /// </summary>
-        public ICollection<ProductDB> Products { get; set; }
+        public virtual ICollection<ProductDB> Products { get; set; }
     }
 }
