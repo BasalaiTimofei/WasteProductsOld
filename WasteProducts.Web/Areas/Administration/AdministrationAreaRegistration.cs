74bd9253b20ee41f034a39b637c6dc832953fc25
Aleksandr Golgovskii <aleksandr.golgovskii@outlook.com>
2018-08-20 15:00:06 +0300

    add administration area with ErrorManagement controller? that helps to control access for Elmah errors page

---
+++ WasteProducts.Web/Areas/Administration/AdministrationAreaRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace WasteProducts.Web.Areas.Administration
+{
+    public class AdministrationAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Administration";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            #region ErrorControllerRoute
+
+            context.MapRoute(
+                name: "Administration.Error",
+                url: "Administration/ErrorManagement/{resource}",
+                defaults: new { controller = "ErrorManagement", action = "Index", resource = UrlParameter.Optional });
+
+            context.MapRoute(
+                name: "Administration.Error.Detail",
+                url: "Administration/ErrorManagement/detail/{resource}",
+                defaults: new { controller = "ErrorManagement", action = "Detail", resource = UrlParameter.Optional });
+
+            #endregion
+
+            context.MapRoute(
+                name: "Administration_default",
+                url: "Administration/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WasteProducts.Web/Areas/Administration/Controllers/ErrorManagementController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using NLog;
+using WasteProducts.Web.Controllers;
+using WasteProducts.Web.Utils.ActionResults;
+
+namespace WasteProducts.Web.Areas.Administration.Controllers
+{
+
+    public class ErrorManagementController : Controller
+    {
+        public ActionResult Index(string resource)
+        {
+            return new ElmahResult();
+        }
+
+        public ActionResult Detail(string resource)
+        {
+            return new ElmahResult();
+        }
+    }
+}
