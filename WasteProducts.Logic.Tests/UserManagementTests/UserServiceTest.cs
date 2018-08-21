82bb80762a7974530f1ce57c481917a101c371be
Alexander Yashyn <yashyn.alexander@gmail.com>
2018-08-21 21:37:28 +0300

    4 Unit Tests for UserService (RegisterAsync method)

--- WasteProducts.Logic.Tests/UserManagementTests/UserServiceTest.cs
+++ WasteProducts.Logic.Tests/UserManagementTests/UserServiceTest.cs
@@ -16,102 +16,201 @@ using WasteProducts.Logic.Services;
 using WasteProducts.Logic.Services.MailService;
 using AutoMapper;
 using WasteProducts.Logic.Common.Models.Users;
+using WasteProducts.Logic.Common.Services.UserService;
+using WasteProducts.Logic.Services.UserService;
+using System.Net.Mail;
 
 namespace WasteProducts.Logic.Tests.UserManagementTests
 {
     [TestFixture]
     public class UserServiceTest
     {
-        private Mock<IUserService> userServicemock;
-        private Mock<IUserRepository> userRepository;
-        private Mock<IMailService> mailService;
+        //private Mock<IUserService> userServicemock;
+        private Mock<IUserRepository> userRepoMock;
+        private Mock<IMailService> mailServiceMock;
+        private IMapper mapper;
         private UserService userService;
 
         [OneTimeSetUp]
         public void TestFixtureSetup()
         {
-            userServicemock = new Mock<IUserService>();
+            //    userServicemock = new Mock<IUserService>();
 
 
-            var userDB = new UserDB()
-            {
-                Id = "1",
-                Email = "Email",
-                UserName = "userName",
-                Created = DateTime.Now.AddYears(-1),
-                Modified = DateTime.Now.AddMinutes(-1)
-            };
-            var user = new User()
-            {
-                Id = "1",
-                Email = "Email",
-                UserName = "userName"
-            };
+            //    var userDB = new UserDB()
+            //    {
+            //        Id = "1",
+            //        Email = "Email",
+            //        UserName = "userName",
+            //        Created = DateTime.Now.AddYears(-1),
+            //        Modified = DateTime.Now.AddMinutes(-1)
+            //    };
+            //    var user = new User()
+            //    {
+            //        Id = "1",
+            //        Email = "Email",
+            //        UserName = "userName"
+            //    };
 
-            userRepository = new Mock<IUserRepository>();
-            #region userRepository SetUp
-            userRepository.Setup(a => a.Select("Does not exist")).Returns((UserDB) null);
-            userRepository.Setup(a => a.Select("Exists")).Returns(userDB);
-            #endregion
-
-
+            //    userRepository = new Mock<IUserRepository>();
+            //    #region userRepository SetUp
+            //    userRepository.Setup(a => a.Select("Does not exist")).Returns((UserDB) null);
+            //    userRepository.Setup(a => a.Select("Exists")).Returns(userDB);
+            //    #endregion
 
 
-            mailService = new Mock<IMailService>();
-            mailService.Setup(a => a.
-            Send(It.IsAny<string>(), 
-                 It.IsAny<string>(), 
-                 It.IsAny<string>())).
-                 Verifiable();
-
-            userService = new UserService(mailService.Object, userRepository.Object);
 
 
-            var mapperMock = new Mock<IMapper>();
-            mapperMock.Setup(a => a.Map<User>(It.IsAny<UserDB>())).
-                Returns(user);
+            //    mailService = new Mock<IMailService>();
+            //    mailService.Setup(a => a.
+            //    Send(It.IsAny<string>(), 
+            //         It.IsAny<string>(), 
+            //         It.IsAny<string>())).
+            //         Verifiable();
 
+            //    userService = new UserService(mailService.Object, userRepository.Object);
+
+
+            //    
+            mailServiceMock = new Mock<IMailService>();
+            userRepoMock = new Mock<IUserRepository>();
+            userService = new UserService(mailServiceMock.Object, userRepoMock.Object);
+            //mapper = new Mapper<UserDB>(User);
+            Mapper.Initialize(cfg => {
+                cfg.CreateMap<UserDB, User>().ReverseMap();
+            });
         }
 
-
         [Test]
-        public void PasswordRequest_NotExistingEmailReturnsFalse_Test()
+        public void UserServiceTest_01_RegisterAsync_Password_and_Confirmation_Doesnt_Match_Returns_Null()
         {
             // arrange
-            bool expected = false;
+            var validEmail = "validEmail@gmail.com";
+            mailServiceMock.Setup(a => a.IsValidEmail(validEmail)).Returns(true);
+            var password = "password";
+            var passwordConfirmationDoesntMatch = "doesn't match";
+            var userNameValid = "validUserName";
+            User expected = null;
+
             // act
-            var actual = userService.PasswordRequest("Does not exist");
+            var actual = userService.RegisterAsync(validEmail, 
+                userNameValid, password, 
+                passwordConfirmationDoesntMatch)
+                .GetAwaiter().GetResult();
+
             // assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void PasswordRequest_ExistingEmailMailServiceSendMethodIsEvoked_Test()
+        public void UserServiceTest_02_RegisterAsync_Email_Is_Not_Valid_Returns_Null()
         {
             // arrange
+            var invalidEmail = "invalidEmail@gmail.com";
+            mailServiceMock.Setup(a => a.IsValidEmail(invalidEmail)).Returns(false);
+            var password = "password";
+            var passwordConfirmationMatch = "password";
+            var userNameValid = "validUserName";
+            User expected = null;
+
+            // act
+            var actual = userService.RegisterAsync(invalidEmail,
+                userNameValid, password, passwordConfirmationMatch)
+                .GetAwaiter().GetResult();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void UserServiceTest_03_RegisterAsync_Confirmation_and_Email_Invalid_Returns_Null()
+        {
+            // arrange
+            var invalidEmail = "invalidEmail@gmail.com";
+            mailServiceMock.Setup(a => a.IsValidEmail(invalidEmail)).Returns(false);
+            var password = "password";
+            var passwordConfirmationDoesntMatch = "doesn't match";
+            var userNameValid = "validUserName";
+            User expected = null;
+
+            // act
+            var actual = userService.RegisterAsync(invalidEmail,
+                userNameValid, password, passwordConfirmationDoesntMatch)
+                .GetAwaiter().GetResult();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void UserServiceTest_04_RegisterAsync_Successful_Register_Returns_Task_User()
+        {
+            // arrange
+            var validEmail = "validEmail@gmail.com";
+            mailServiceMock.Setup(a => a.IsValidEmail(validEmail)).Returns(true);
+            var password = "password";
+            var passwordConfirmationMatch = "password";
+            var userNameValid = "validUserName";
+            User expectedUser = new User();
+            var expectedEmail = validEmail;
+            var expectedPassword = password;
+            var expectedUserName = userNameValid;
+            userRepoMock.Setup(a => a.AddAsync(It.IsAny<UserDB>())).Returns((Task)null);
+            userRepoMock.Setup(b => b.Select(It.Is<string>(c => c == validEmail),
+                It.IsAny<bool>())).Returns(Mapper.Map<UserDB>(expectedUser));
+
+            // act
+            var actualUser = userService.RegisterAsync(validEmail,
+                userNameValid, password, passwordConfirmationMatch)
+                .GetAwaiter().GetResult();
+
+            // assert
+            Assert.AreSame(expectedUser, actualUser);
+            Assert.AreEqual(expectedEmail, actualUser.Email);
+            Assert.AreEqual(expectedPassword, actualUser.Password);
+            Assert.AreEqual(expectedUserName, actualUser.UserName);
+        }
+
+
+
+        //[Test]
+        //public void PasswordRequest_NotExistingEmailReturnsFalse_Test()
+        //{
+        //    // arrange
+        //    bool expected = false;
+        //    // act
+        //    var actual = userService.PasswordRequest("Does not exist");
+        //    // assert
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+        //[Test]
+        //public void PasswordRequest_ExistingEmailMailServiceSendMethodIsEvoked_Test()
+        //{
+        //    // arrange
             
-            // act
-            userService.PasswordRequest("Exists");
+        //    // act
+        //    userService.PasswordRequest("Exists");
 
-            // assert
-            mailService.Verify(a => a.Send(It.IsAny<string>(),
-                 It.IsAny<string>(),
-                 It.IsAny<string>()), Times.Once());
-        }
+        //    // assert
+        //    mailService.Verify(a => a.Send(It.IsAny<string>(),
+        //         It.IsAny<string>(),
+        //         It.IsAny<string>()), Times.Once());
+        //}
 
-        [Test]
-        public void PasswordRequest_ExistingEmailMailServiceReturnsTrue_Test()
-        {
-            // arrange
-            bool expected = true;
+        //[Test]
+        //public void PasswordRequest_ExistingEmailMailServiceReturnsTrue_Test()
+        //{
+        //    // arrange
+        //    bool expected = true;
 
-            // act 
-            var actual = userService.PasswordRequest("Exists");
+        //    // act 
+        //    var actual = userService.PasswordRequest("Exists");
 
-            // assert
-            Assert.AreEqual(expected, actual);
+        //    // assert
+        //    Assert.AreEqual(expected, actual);
 
-        }
+        //}
 
     }
 }
