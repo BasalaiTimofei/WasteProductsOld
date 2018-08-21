5e7ca55c68cd65a0ec164626ef23dda09a1cf01a
andrey <wepret@yandex.ru>
2018-08-21 21:45:21 +0300

    Added documentation into WasteProducts.DataAccess.Common.Repositories.Security

--- WasteProducts.DataAccess.Common/Repositories/Security/IRepositoryBase.cs
+++ WasteProducts.DataAccess.Common/Repositories/Security/IRepositoryBase.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Threading;
-using System.Threading.Tasks;
-using WasteProducts.DataAccess.Common.Models;
-using WasteProducts.DataAccess.Common.Models.Security.Infrastructure;
+﻿using WasteProducts.DataAccess.Common.Models.Security.Infrastructure;
 
 namespace WasteProducts.DataAccess.Common.Repositories.Security
 {
+    /// <summary>
+    /// Interface IRepositoryBase for the IRepository. Has an inheritance from IRepository
+    /// </summary>
     public interface IRepositoryBase<TEntity> : IRepository<TEntity> where TEntity : class
     {
        
--- WasteProducts.DataAccess.Common/Repositories/Security/IUserRoleRepository.cs
+++ WasteProducts.DataAccess.Common/Repositories/Security/IUserRoleRepository.cs
@@ -2,8 +2,17 @@
 
 namespace WasteProducts.DataAccess.Common.Repositories.Security
 {
+    /// <summary>
+    /// Interface IUserRoleRepository for the UserRoleRepository.
+    /// </summary>
     public interface IUserRoleRepository
     {
+        /// <summary>
+        /// Checking if user with current id has role with current id role
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="roleId"></param>
+        /// <returns>bool value as task</returns>
         Task<bool> IsInRoleAsync(int userId, int roleId);
     }
 }
