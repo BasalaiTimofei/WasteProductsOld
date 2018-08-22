d2c13c9f512e7487a70808edf914f45c6ce204e1
Alexander Yashyn <yashyn.alexander@gmail.com>
2018-08-22 19:14:44 +0300

    Delete commented code in UserServiceTest

--- WasteProducts.Logic.Tests/UserManagementTests/UserServiceTest.cs
+++ WasteProducts.Logic.Tests/UserManagementTests/UserServiceTest.cs
@@ -26,61 +26,27 @@ namespace WasteProducts.Logic.Tests.UserManagementTests
     [TestFixture]
     public class UserServiceTest
     {
-        //private Mock<IUserService> userServicemock;
         private Mock<IUserRepository> userRepoMock;
         private Mock<IMailService> mailServiceMock;
-        private IMapper mapper;
         private UserService userService;
 
         [OneTimeSetUp]
         public void TestFixtureSetup()
         {
-            //    userServicemock = new Mock<IUserService>();
-
-
-            //    var userDB = new UserDB()
-            //    {
-            //        Id = "1",
-            //        Email = "Email",
-            //        UserName = "userName",
-            //        Created = DateTime.Now.AddYears(-1),
-            //        Modified = DateTime.Now.AddMinutes(-1)
-            //    };
-            //    var user = new User()
-            //    {
-            //        Id = "1",
-            //        Email = "Email",
-            //        UserName = "userName"
-            //    };
-
-            //    userRepository = new Mock<IUserRepository>();
-            //    #region userRepository SetUp
-            //    userRepository.Setup(a => a.Select("Does not exist")).Returns((UserDB) null);
-            //    userRepository.Setup(a => a.Select("Exists")).Returns(userDB);
-            //    #endregion
-
-
-
-
-            //    mailService = new Mock<IMailService>();
-            //    mailService.Setup(a => a.
-            //    Send(It.IsAny<string>(), 
-            //         It.IsAny<string>(), 
-            //         It.IsAny<string>())).
-            //         Verifiable();
-
-            //    userService = new UserService(mailService.Object, userRepository.Object);
-
-
-            //    
             mailServiceMock = new Mock<IMailService>();
             userRepoMock = new Mock<IUserRepository>();
             userService = new UserService(mailServiceMock.Object, userRepoMock.Object);
 
-            //Mapper.Initialize(cfg => 
-            //{
-            //    cfg.AddProfile(new UserProfile()); ;
-            //});
+            Mapper.Initialize(cfg => 
+            {
+                cfg.AddProfile(new UserProfile()); ;
+            });
+        }
+
+        [OneTimeTearDown]
+        public void TestFixtureTearDown()
+        {
+            Mapper.Reset();
         }
 
         [Test]
@@ -180,46 +146,6 @@ namespace WasteProducts.Logic.Tests.UserManagementTests
             Assert.AreEqual(expectedUserName, actualUser.UserName);
         }
 
-
-
-        //[Test]
-        //public void PasswordRequest_NotExistingEmailReturnsFalse_Test()
-        //{
-        //    // arrange
-        //    bool expected = false;
-        //    // act
-        //    var actual = userService.PasswordRequest("Does not exist");
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-        //}
-
-        //[Test]
-        //public void PasswordRequest_ExistingEmailMailServiceSendMethodIsEvoked_Test()
-        //{
-        //    // arrange
-            
-        //    // act
-        //    userService.PasswordRequest("Exists");
-
-        //    // assert
-        //    mailService.Verify(a => a.Send(It.IsAny<string>(),
-        //         It.IsAny<string>(),
-        //         It.IsAny<string>()), Times.Once());
-        //}
-
-        //[Test]
-        //public void PasswordRequest_ExistingEmailMailServiceReturnsTrue_Test()
-        //{
-        //    // arrange
-        //    bool expected = true;
-
-        //    // act 
-        //    var actual = userService.PasswordRequest("Exists");
-
-        //    // assert
-        //    Assert.AreEqual(expected, actual);
-
-        //}
 
     }
 }
