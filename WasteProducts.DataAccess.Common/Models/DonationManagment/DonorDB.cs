c11a17a712fb701bd7915da1b2fd785eb7df6a41
Andrew Palcheuski <timplayer@tut.by>
2018-08-22 18:44:02 +0300

    Add DonorDB class.

---
+++ WasteProducts.DataAccess.Common/Models/DonationManagment/DonorDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WasteProducts.DataAccess.Common.Models.DonationManagment
+{
+    class DonorDB
+    {
+        /// <summary>
+        /// Unique donor ID.
+        /// </summary>
+        [Key]
+        string Id { get; set; }
+
+        /// <summary>
+        /// Donor's primary email address.
+        /// </summary>
+        string Email { get; set; }
+
+        /// <summary>
+        /// That either a donor verified or not.
+        /// </summary>
+        bool IsVerified { get; set; }
+
+        /// <summary>
+        /// Account holder's first name.
+        /// </summary>
+        string FirstName { get; set; }
+
+        /// <summary>
+        /// Account holder's last name.
+        /// </summary>
+        string LastName { get; set; }
+
+        /// <summary>
+        /// Address of donor.
+        /// </summary>
+        AddressDB Address { get; set; }
+
+        /// <summary>
+        /// Specifies the timestamp for creating of a specific donor in the database.
+        /// </summary>
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Specifies the timestamp for modifying of a specific donor in the database.
+        /// </summary>
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
