5d33b83b944d335869fe1b0efa6df276a4131096
Andrew Palcheuski <timplayer@tut.by>
2018-08-22 19:07:43 +0300

    Add IDonationLogger interface.

---
+++ WasteProducts.Logic.Common/Services/IDonationLogger.cs
@@ -0,0 +1,9 @@
+﻿using WasteProducts.Logic.Common.Models.DonationManagment;
+
+namespace WasteProducts.Logic.Common.Services
+{
+    interface IDonationLogger
+    {
+        void Log(Donation donation);
+    }
+}
