eea59984aef5d1c5747b88aeac5c66c9af82e72b
Dr9vik <Dr9virane@gmail.com>
2018-08-22 22:56:08 +0300

    fix

--- WasteProducts.DataAccess.Common/Models/Groups/GroupProductDB.cs
+++ WasteProducts.DataAccess.Common/Models/Groups/GroupProductDB.cs
@@ -13,9 +13,9 @@ namespace WasteProducts.DataAccess.Common.Models
         /// </summary>
         public int Id { get; set; }
         /// <summary>
-        /// GroupBordId - secondary key
+        /// GroupBoardId - secondary key
         /// </summary>
-        public int GroupBordId { get; set; }
+        public int GroupBoardId { get; set; }
         public GroupBoardDB GroupBoardDB { get; set; }
         /// <summary>
         /// ProductId - product which add at board
