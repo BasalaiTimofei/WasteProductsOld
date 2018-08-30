87c7feaff3b7d0434cfe9dc0bf0b00e8312ef1d5
Ilya Sauko <garsavko@mail.ru>
2018-08-30 22:47:59 +0300

    Added nuget package Zxing, System.Drawing. Deleted UserId from BarcodeInfo. Сhanged IBarcodeService.

--- WasteProducts.Logic.Common/Services/IBarcodeService.cs
+++ WasteProducts.Logic.Common/Services/IBarcodeService.cs
@@ -1,22 +1,31 @@
-﻿using WasteProducts.Logic.Common.Models.Barcods;
+﻿using System.Drawing;
+using WasteProducts.Logic.Common.Models.Barcods;
 
 namespace WasteProducts.Logic.Common.Services
 {
     public interface IBarcodeService
     {
         /// <summary>
+        /// Resize a image of barcode
+        /// </summary>
+        /// <param name="img"> image of barcode photo</param>
+        /// <param name="width"> width of barcode image result</param>
+        /// <param name="height"> height of barcode image result</param>
+        /// <returns>Resized image</returns>
+        Image Resize(Image img, int width, int height);
+
+        /// <summary>
         /// get a numerical barcode on the photo
         /// </summary>
-        /// <param name="filePath"> path to barcode photo</param>
-        /// <param name="userId"> User Id by User</param>
-        /// <returns>a numerical barcode</returns>
-        BarcodeInfo GetCode(string filePath, int userId);
+        /// <param name="image"> image of barcode photo</param>
+        /// <returns>string of a numerical barcode</returns>
+        BarcodeInfo ScanByZxing(Image image);
 
         /// <summary>
         /// get product information
         /// </summary>
         /// <param name="barcodeInfo">a model of BarcodeInfo</param>
         /// <returns>product information in the format Barcode</returns>
-        Barcode GetBarcode(BarcodeInfo barcodeInfo);
+        //Barcode GetBarcode(BarcodeInfo barcodeInfo);
     }
 }
