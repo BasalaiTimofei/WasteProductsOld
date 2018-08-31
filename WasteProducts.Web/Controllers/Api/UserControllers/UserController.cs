5ffcfadd1882ae214f75c55a070fa33620d6b15e
Alexander Yashyn <yashyn.alexander@gmail.com>
2018-08-31 18:40:51 +0300

    sync commit

--- WasteProducts.Web/Controllers/Api/UserControllers/UserController.cs
+++ WasteProducts.Web/Controllers/Api/UserControllers/UserController.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using NLog;
+using WasteProducts.Logic.Services.UserService;
 
 namespace WasteProducts.Web.Controllers.Api
 {
+    [RoutePrefix("Users")]
     public class UserController : BaseApiController
     {
+
+        protected UserController(ILogger logger) : base(logger)
+        {
+        }
+
+
     }
 }
--- WasteProducts.Web/Controllers/Mvc/UserControllers/UserController.cs
+++ WasteProducts.Web/Controllers/Mvc/UserControllers/UserController.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NLog;
 
 namespace WasteProducts.Web.Controllers.Mvc.UserControllers
 {
     public class UserController : BaseMvcController
     {
+        protected UserController(ILogger logger) : base(logger)
+        {
+        }
     }
 }
