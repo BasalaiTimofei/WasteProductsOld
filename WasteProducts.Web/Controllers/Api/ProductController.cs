ae43398af82199439a751b64054d52d8ffd3375a
Mikhail Shishlyannikov <mikhailshishlyannikov@gmail.com>
2018-09-02 23:59:38 +0300

    ProductController was created

---
+++ WasteProducts.Web/Controllers/Api/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NLog;
+using WasteProducts.Logic.Common.Services;
+
+namespace WasteProducts.Web.Controllers.Api
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : BaseApiController
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger logger, IProductService productService) : base(logger)
+        {
+            _productService = productService;
+        }
+        
+
+    }
+}
