c9f3b4f9ec971722b3ba9dee6136347ee4a3eb74
SergeiTishkov <tishkovsergei92@gmail.com>
2018-09-03 14:46:55 +0300

    Merge thrash fixed

--- WasteProducts.DataAccess/Contexts/WasteContext.cs
+++ WasteProducts.DataAccess/Contexts/WasteContext.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using Microsoft.AspNet.Identity.EntityFramework;
 using WasteProducts.DataAccess.Common.Context;
 using WasteProducts.DataAccess.Common.Models.Products;
-using WasteProducts.DataAccess.Common.Models;
 using WasteProducts.DataAccess.Common.Models.Users;
 using WasteProducts.DataAccess.Contexts.Config;
 using WasteProducts.DataAccess.Common.Models.Groups;
@@ -58,22 +57,16 @@ namespace WasteProducts.DataAccess.Contexts
         ///  create, read, update, delete and to get product list operations in 'ProductRepository' class.
         /// </summary>
         public IDbSet<ProductDB> Products { get; set; }
-
         /// <summary>
         /// Property added for to use an entity set that is used to perform
         ///  create, read, update, delete and to get category list operations in 'CategoryRepository' class.
         /// </summary>
-        /// 
         public IDbSet<CategoryDB> Categories { get; set; }
 
         public IDbSet<GroupBoardDB> GroupBordDBs { get; set; }
-
         public IDbSet<GroupDB> GroupDBs { get; set; }
-
         public IDbSet<GroupUserDB> GroupUserDBs { get; set; }
-
         public IDbSet<GroupCommentDB> GroupCommentDBs { get; set; }
-
         public IDbSet<GroupProductDB> GroupProductDBs { get; set; }
     }
 }
