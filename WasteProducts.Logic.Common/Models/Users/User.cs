adf1e3bd40d297af47d2335910c8f0ce0cbb0f83
SergeiTishkov <tishkovsergei92@gmail.com>
2018-09-08 16:10:17 +0300

    Sorry, wrong branch)))

--- WasteProducts.Logic.Common/Models/Users/User.cs
+++ WasteProducts.Logic.Common/Models/Users/User.cs
@@ -43,49 +43,9 @@ namespace WasteProducts.Logic.Common.Models.Users
         public virtual string Email { get; set; }
 
         /// <summary>
-        /// True if the email is confirmed, default is false.
-        /// </summary>
-        public virtual bool EmailConfirmed { get; set; }
-
-        /// <summary>
-        /// User's hashed password.
-        /// </summary>
-        public virtual string PasswordHash { get; set; }
-
-        /// <summary>
-        /// A random value that should change whenever a users credentials have changed (password changed, login removed)
-        /// </summary>
-        public virtual string SecurityStamp { get; set; }
-
-        /// <summary>
         /// PhoneNumber for the user.
         /// </summary>
         public virtual string PhoneNumber { get; set; }
-
-        /// <summary>
-        /// True if the phone number is confirmed, default is false.
-        /// </summary>
-        public virtual bool PhoneNumberConfirmed { get; set; }
-
-        /// <summary>
-        /// Is two factor enabled for the user.
-        /// </summary>
-        public virtual bool TwoFactorEnabled { get; set; }
-
-        /// <summary>
-        /// DateTime in UTC when lockout ends, any time in the past is considered not locked out.
-        /// </summary>
-        public virtual DateTime? LockoutEndDateUtc { get; set; }
-
-        /// <summary>
-        /// Is lockout enabled for this userю
-        /// </summary>
-        public virtual bool LockoutEnabled { get; set; }
-
-        /// <summary>
-        /// Used to record failures for the purposes of lockout.
-        /// </summary>
-        public virtual int AccessFailedCount { get; set; }
 
         /// <summary>
         /// All user roles.
