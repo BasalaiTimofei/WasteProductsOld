ff1319f2417d30077204c255f3d0c453885ac966
Andrew Palcheuski <timplayer@tut.by>
2018-09-10 16:06:58 +0300

    Add IPNController.

---
+++ WasteProducts.Web/Controllers/Api/IPNController.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+using Swagger.Net.Annotations;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WasteProducts.Logic.Common.Services;
+
+namespace WasteProducts.Web.Controllers.Api
+{
+    /// <summary>
+    /// Controller that receives Instant Payment Notifications from PayPal.
+    /// </summary>
+    [RoutePrefix("api/IPN")]
+    public class IPNController : BaseApiController
+    {
+        private IDonationService _donationService { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="donationService">Donation service</param>
+        /// <param name="logger">NLog logger</param>
+        public IPNController(IDonationService donationService, ILogger logger) : base(logger)
+        {
+            _donationService = donationService;
+        }
+
+        /// <summary>
+        /// Receives Instant Payment Notifications from PayPal.
+        /// </summary>
+        [SwaggerResponseRemoveDefaults]
+        [HttpPost, Route("receive")]
+        public OkResult Receive([FromUri]string query)
+        {
+            return Ok();
+        }
+    }
+}
