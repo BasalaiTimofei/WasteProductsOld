b8463f0499d6d7aa616b431185bd441a92caba7d
aleks <bad.bad.jaguar@gmail.com>
2018-09-12 21:38:20 +0300

    Added Validator file

---
+++ WasteProducts.Logic/Validators/Products/ProductsValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentAssertions;
+using WasteProducts.Logic.Common.Models.Products;
+
+namespace WasteProducts.Logic.Validators.Products
+{
+    class ProductsValidator : AbstractValidator<Product>
+    {
+        public ProductsValidator()
+        {
+        }
+    }
+}
