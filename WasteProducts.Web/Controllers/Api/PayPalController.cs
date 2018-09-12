cb65280ab76b9073e1d6d5ce2a7a9430aff1332d
Andrew Palcheuski <timplayer@tut.by>
2018-09-12 15:51:55 +0300

    Add PayPalController implementation with IPN protocol.

--- WasteProducts.Web/Controllers/Api/PayPalController.cs
+++ WasteProducts.Web/Controllers/Api/PayPalController.cs
@@ -1,6 +1,11 @@
 ﻿using NLog;
 using Swagger.Net.Annotations;
+using System.Collections.Specialized;
+using System.IO;
 using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Results;
 using WasteProducts.Logic.Common.Services.Donations;
@@ -33,7 +38,76 @@ namespace WasteProducts.Web.Controllers.Api
         [HttpPost, Route("donation/log")]
         public OkResult Receive([FromUri]string query)
         {
+            var context = new HttpContextWrapper(HttpContext.Current);
+            HttpRequestBase request = context.Request;
+            Task.Run(() => VerifyRequest(request));
             return Ok();
+        }
+
+        /// <summary>
+        /// Verifies that the request came from PayPal.
+        /// </summary>
+        private void VerifyRequest(HttpRequestBase payPalRequest)
+        {
+            var param = payPalRequest.BinaryRead(payPalRequest.ContentLength);
+            var payPalRequestString = Encoding.ASCII.GetString(param);
+
+            HttpWebRequest verificationRequest = PrepareVerificationRequest(payPalRequestString);
+
+            // Send the request to PayPal and get the response
+            string verificationResponse = null;
+            using (var streamIn = new StreamReader(verificationRequest.GetResponse().GetResponseStream()))
+                verificationResponse = streamIn.ReadToEnd();
+
+            ProcessVerificationResponse(verificationResponse, payPalRequestString);
+        }
+
+        /// <summary>
+        /// Prepares a verification request.
+        /// </summary>
+        private HttpWebRequest PrepareVerificationRequest(string payPalRequestString)
+        {
+            const string PAYPAL_URL = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+            const string VERIFICATION_PREFIX = "cmd=_notify-validate&";
+            const string POST = "POST";
+            const string CONTENT_TYPE = "application/x-www-form-urlencoded";
+
+            HttpWebRequest verificationRequest = (HttpWebRequest)WebRequest.Create(PAYPAL_URL);
+
+            // Set values for the verification request
+            verificationRequest.Method = POST;
+            verificationRequest.ContentType = CONTENT_TYPE;
+
+            // Add cmd=_notify-validate to the payload
+            string verificationString = VERIFICATION_PREFIX + payPalRequestString;
+            verificationRequest.ContentLength = verificationString.Length;
+
+            // Attach payload to the verification request
+            using (var streamOut = new StreamWriter(verificationRequest.GetRequestStream(), Encoding.ASCII))
+                streamOut.Write(verificationString);
+
+            return verificationRequest;
+        }
+
+        private void ProcessVerificationResponse(string verificationResponse, string payPalRequestString)
+        {
+            const string INVALID = "INVALID";
+            const string PAYMENT_STATUS = "payment_status";
+            const string COMPLETED = "Completed";
+
+            if (verificationResponse.Equals(INVALID))
+                return;
+
+            // check that Payment_status=Completed
+            // check that Txn_id has not been previously processed
+            // check that Receiver_email is your Primary PayPal email
+            // check that Payment_amount/Payment_currency are correct
+            // process payment
+            NameValueCollection payPalArguments = HttpUtility.ParseQueryString(payPalRequestString);
+            //.  more args as needed look at the list from paypal IPN doc
+            if (payPalArguments[PAYMENT_STATUS] != COMPLETED)
+                return;
+            string user_email = payPalArguments["payer_email"];            
         }
     }
 }
