a638a781afc2b57af0781bd18bab9c67b0d39591
Aleksandr Golgovskii <aleksandr.golgovskii@outlook.com>
2018-09-12 03:57:12 +0300

    fix Database, DbService, DbController added resource file for const strings

--- WasteProducts.Logic.Common/Models/Diagnostic/DatabaseState.cs
+++ WasteProducts.Logic.Common/Models/Diagnostic/DatabaseState.cs
@@ -1,42 +1,36 @@
-﻿namespace WasteProducts.Logic.Common.Models.Diagnostic
+﻿using System.Collections.Generic;
+
+namespace WasteProducts.Logic.Common.Models.Diagnostic
 {
     /// <summary>
-    /// Model for database status response
+    /// Model for database state response
     /// </summary>
-    public class DatabaseStatus
+    public class DatabaseState
     {
         /// <summary>
         /// Default constructor
         /// </summary>
-        public DatabaseStatus() { }
+        public DatabaseState() { }
 
         /// <summary>
         /// Constructor with params
         /// </summary>
-        /// <param name="isDbExist"></param>
-        /// <param name="isDbCompatibleWithModel"></param>
-        public DatabaseStatus(bool isDbExist, bool isDbCompatibleWithModel)
+        /// <param name="isExist"></param>
+        /// <param name="isCompatibleWithModel"></param>
+        public DatabaseState(bool isExist, bool isCompatibleWithModel)
         {
-            IsDbExist = isDbExist;
-            IsDbCompatibleWithModel = isDbCompatibleWithModel;
+            IsExist = isExist;
+            IsCompatibleWithModel = isCompatibleWithModel;
         }
 
         /// <summary>
         /// "True" if database already exist, in otherwise "False".
         /// </summary>
-        public bool IsDbExist { get; set; }
+        public bool IsExist { get; set; }
 
         /// <summary>
         /// "True" if database compatible with model in code, in otherwise "False".
         /// </summary>
-        public bool IsDbCompatibleWithModel { get; set; }
-
-        /// <inheritdoc />
-        public override bool Equals(object obj)
-        {
-            return obj is DatabaseStatus databaseStatusObj 
-                   && this.IsDbExist == databaseStatusObj.IsDbExist 
-                   && this.IsDbCompatibleWithModel == databaseStatusObj.IsDbCompatibleWithModel;
-        }
+        public bool IsCompatibleWithModel { get; set; }
     }
 }
--- WasteProducts.Logic.Common/Services/Diagnostic/IDbSeedService.cs
+++ WasteProducts.Logic.Common/Services/Diagnostic/IDbSeedService.cs
@@ -1,12 +1,10 @@
 ﻿using System.Threading.Tasks;
-using WasteProducts.Logic.Common.Attributes;
 
 namespace WasteProducts.Logic.Common.Services.Diagnostic
 {
     /// <summary>
     /// Service, that seeds database with permanent and test entries.
     /// </summary>
-    [Trace]
     public interface IDbSeedService
     {
         /// <summary>
