e851cac433228d2e890a7925d5ba5439375bc0d3
SergeiTishkov <tishkovsergei92@gmail.com>
2018-09-13 22:53:38 +0300

    Some XML documentations

--- WasteProducts.Web/Controllers/Api/BaseApiController.cs
+++ WasteProducts.Web/Controllers/Api/BaseApiController.cs
@@ -3,13 +3,23 @@ using NLog;
 
 namespace WasteProducts.Web.Controllers.Api
 {
+    /// <summary>
+    /// Abstract class-parent for all API controllers of WasteContext.Web
+    /// </summary>
     public abstract class BaseApiController : ApiController
     {
+        /// <summary>
+        /// Constructor needed for logging purposes.
+        /// </summary>
+        /// <param name="logger"></param>
         protected BaseApiController(ILogger logger)
         {
             Logger = logger;
         }
 
+        /// <summary>
+        /// Logger of the controller.
+        /// </summary>
         protected ILogger Logger { get; }
     }
 }
--- WasteProducts.Web/Controllers/Mvc/BaseMvcController.cs
+++ WasteProducts.Web/Controllers/Mvc/BaseMvcController.cs
@@ -3,13 +3,23 @@ using NLog;
 
 namespace WasteProducts.Web.Controllers.Mvc
 {
+    /// <summary>
+    /// Abstract class-parent for all non-API controllers of WasteContext.Web
+    /// </summary>
     public abstract class BaseMvcController : Controller
     {
+        /// <summary>
+        /// Constructor needed for logging purposes.
+        /// </summary>
+        /// <param name="logger"></param>
         protected BaseMvcController(ILogger logger)
         {
             Logger = logger;
         }
 
+        /// <summary>
+        /// Logger of the controller.
+        /// </summary>
         protected ILogger Logger { get; }
     }
 }
