9d1a8e375a31a8d25ba0e7a84f824c5753f16411
Aleksandr Golgovskii <aleksandr.golgovskii@outlook.com>
2018-09-13 16:02:57 +0300

    added ng materials module added httpclient module

--- WasteProductsFront/src/app/app.module.ts
+++ WasteProductsFront/src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
+
+import { MaterialModule } from './modules/material/material.module';
+
+/* Components */
 import { AppComponent } from './app.component';
+import { DatabaseComponent } from './components/database/database.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    DatabaseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
