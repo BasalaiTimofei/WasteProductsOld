2efc30e0d8224be9eda8862b6dfdba5f1e861c47
SergeiTishkov <tishkovsergei92@gmail.com>
2018-09-16 21:19:03 +0300

    MainPageComponent added

--- WasteProductsFront/src/app/app.module.ts
+++ WasteProductsFront/src/app/app.module.ts
@@ -10,12 +10,15 @@ import { MaterialModule } from './modules/material/material.module';
 /* Components */
 import { AppComponent } from './app.component';
 import { DatabaseComponent } from './components/database/database.component';
+import { UserMainPageComponent } from './userComponents/user-main-page/user-main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    DatabaseComponent
+    DatabaseComponent,
+
+    UserMainPageComponent
   ],
   imports: [
     BrowserModule, FormsModule,
