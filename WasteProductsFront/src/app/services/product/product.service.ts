255c974a3b96c245e7bfd49bf63f4ffc0116d39f
aleks <bad.bad.jaguar@gmail.com>
2018-09-17 18:50:25 +0300

    some changes in service UI

--- WasteProductsFront/src/app/services/product/product.service.ts
+++ WasteProductsFront/src/app/services/product/product.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  private apiUrl = `${environment.apiHostUrl}/api/product/products`;
 
   constructor() { }
 }
