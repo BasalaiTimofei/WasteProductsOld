a6689ea28df45002ec5b8c91dfacaf331017b539
Aleksandr Golgovskii <aleksandr.golgovskii@outlook.com>
2018-09-24 17:44:19 +0300

    addded notification controller, added notification signalr notification provider

--- WasteProducts.Web/Controllers/Api/NotificationController.cs
+++ WasteProducts.Web/Controllers/Api/NotificationController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using Ninject.Extensions.Logging;
 using Swagger.Net.Annotations;
+using WasteProducts.Logic.Common.Models.Notifications;
 using WasteProducts.Logic.Common.Services.Notifications;
 
 namespace WasteProducts.Web.Controllers.Api
@@ -13,17 +15,27 @@ namespace WasteProducts.Web.Controllers.Api
     /// <summary>
     /// Api controller for database management
     /// </summary>
-    [RoutePrefix("api/")]
+    [RoutePrefix("api")]
     [SwaggerResponse(HttpStatusCode.BadRequest, "Incorrect query string")]
     [SwaggerResponse(HttpStatusCode.Unauthorized, "Unauthorized request.")]
     [SwaggerResponse(HttpStatusCode.InternalServerError, "Exceptions during the process.")]
     public class NotificationController : BaseApiController
     {
+        private readonly INotificationService _notificationService;
+
         /// <inheritdoc />
         public NotificationController(INotificationService notificationService, ILogger logger) : base(logger)
         {
+            _notificationService = notificationService;
         }
 
-        [HttpPost, Route("")]
+        [HttpPost, Route("user/{userId}/notificate")]
+        [SwaggerResponse(HttpStatusCode.NoContent, "Notification was sent.")]
+        public async Task<IHttpActionResult> NotificateUser(string userId, [FromBody] Notification notification)
+        {
+            await _notificationService.NotificateUserAsync(userId, notification).ConfigureAwait(true);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
     }
 }
