db1cb68f2b5625ad5a03d3402c840cb781253b0c
Aleksandr Golgovskii <aleksandr.golgovskii@outlook.com>
2018-09-25 17:10:18 +0300

    added BaseService with logging methods, added BaseHttpService with HttpClient and handleError method

--- WasteProductsFront/src/app/services/product/product.service.ts
+++ WasteProductsFront/src/app/services/product/product.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BaseHttpService } from '../base/base-http.service';
+import { LoggingService } from '../logging/logging.service';
+
+// environment
 import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
-  getProductSet(): any {
-    throw new Error("Method not implemented."); //change to correct method
-  }
+export class ProductService extends BaseHttpService {
 
   private apiUrl = `${environment.apiHostUrl}/api/product/products`;
 
-  constructor() { }
+  constructor(httpService: HttpClient, loggingService: LoggingService) {
+    super(httpService, loggingService);
+  }
+
+  getProductSet(): any {
+    throw new Error('Method not implemented.'); // change to correct method
+  }
 }
--- WasteProductsFront/src/app/services/search/search.service.ts
+++ WasteProductsFront/src/app/services/search/search.service.ts
@@ -2,25 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { SearchProduct } from '../../models/search-product';
 import { UserQuery } from '../../models/top-query';
-import { Observable, of } from 'rxjs';
-import { catchError, tap, map } from 'rxjs/operators';
+import { Observable} from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { environment } from '../../../environments/environment.prod';
 import { LoggingService } from '../logging/logging.service';
+import { BaseHttpService } from '../base/base-http.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class SearchService {
+export class SearchService extends BaseHttpService {
   searchProducts: SearchProduct[];
   private URL_SEARCH = `${environment.apiHostUrl}/api/search`;  // URL to web api
 
-  constructor(
-    private http: HttpClient,
-    private logService: LoggingService ) { }
+  constructor(httpService: HttpClient, loggingService: LoggingService) {
+    super(httpService, loggingService);
+   }
 
   getDefault(query: string): Observable<SearchProduct[]> {
-    return this.http.get<SearchProduct[]>(this.URL_SEARCH + '/products/default', { params: new HttpParams().set('query', query)}).pipe(
+    return this.httpService.get<SearchProduct[]>(this.URL_SEARCH + '/products/default', { params: new HttpParams().set('query', query)}).pipe(
       map(res => {
         const result: any = res;
         return result.map((item) => new SearchProduct(item.Id, item.Name));
@@ -29,7 +30,7 @@ export class SearchService {
   }
 
   getTopSearchQueries(query: string): Observable<UserQuery[]> {
-    return this.http.get<UserQuery[]>(this.URL_SEARCH + '/queries', { params: new HttpParams().set('query', query)}).pipe(
+    return this.httpService.get<UserQuery[]>(this.URL_SEARCH + '/queries', { params: new HttpParams().set('query', query)}).pipe(
       map(res => {
         const result: any = res;
         return result.map(item => new UserQuery(item.QueryString));
@@ -38,30 +39,6 @@ export class SearchService {
   }
 
   gettest(query: string) {
-    return this.http.get('http://localhost:2189/api/search/products/default?query=' + query, {observe: 'response'});
-  }
-
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-    /** Log with the LoggingService */
-  private log(msg: any) {
-    this.logService.log(`SearchService: ${msg}`);
+    return this.httpService.get('http://localhost:2189/api/search/products/default?query=' + query, {observe: 'response'});
   }
 }
