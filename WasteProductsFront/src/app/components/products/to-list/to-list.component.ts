dd77f7bb4377d8cdfb8c6507a48d5b684e9a227f
SergeiTishkov <tishkovsergei92@gmail.com>
2018-09-27 20:52:35 +0300

    team work commit

--- WasteProductsFront/src/app/components/products/to-list/to-list.component.ts
+++ WasteProductsFront/src/app/components/products/to-list/to-list.component.ts
@@ -13,18 +13,17 @@ import { UserProduct } from '../../../models/users/user-product';
 
 export class ToListComponent implements OnInit {
 
-
   constructor (private productService: ProductService) {}
 
+  products: Product[] = [];
   userProducts: UserProduct[]; // К обсуждению
 
   data: Product[] = this.productService.PRODUCTS_DATA;
-  dataSource = new MatTableDataSource(this.productService.PRODUCTS_DATA);
-  displayedColumns: string[] = ['id', 'name', 'avgRating', 'composition', 'isHidden'];
+  dataSource = new MatTableDataSource(this.data);
+  displayedColumns: string[] = ['Id', 'Name', 'AvgRating', 'Composition', 'IsHidden'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-
 
   ngOnInit() {
     this.paginator.length = this.data.length;
@@ -32,9 +31,18 @@ export class ToListComponent implements OnInit {
     this.dataSource.sort = this.sort;
 
 this.productService.loadUserProducts().subscribe(
-    res => this.userProducts = res, // К обсуждению
+    res => {
+      this.userProducts = res;
+      // tslint:disable-next-line:prefer-const
+      for (let item of res) {
+        item.Product.AvgRating = 3;
+        item.Product.IsHidden = false;
+        this.products.push(item.Product);
+      }
+    } ,
     err => console.error(err));
   }
+
     applyFilter(filterValue: string) {
       this.dataSource.filter = filterValue.trim().toLowerCase();
     }
