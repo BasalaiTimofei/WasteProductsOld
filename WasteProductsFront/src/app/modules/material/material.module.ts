b76c8bc6d771e6d124cffae55ffaf7cc5b0e194a
Aleksandr Golgovskii <aleksandr.golgovskii@outlook.com>
2018-09-27 22:00:26 +0300

    added AccountPanel,  AccountPanelButton components

--- WasteProductsFront/src/app/modules/material/material.module.ts
+++ WasteProductsFront/src/app/modules/material/material.module.ts
@@ -1,23 +1,32 @@
 import { NgModule } from '@angular/core';
 
-import { MatCardModule, MatListModule, MatIconModule, MatDividerModule } from '@angular/material';
+import { MatCardModule, MatListModule, MatIconModule, MatDividerModule, } from '@angular/material';
 import { MatMenuModule, MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule, MatChipsModule } from '@angular/material';
 import { MatSnackBarModule, MatDialogModule, MatTooltipModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 import { MatOptionModule, MatAutocompleteModule, MatTableModule } from '@angular/material';
 import { MatPaginatorModule } from '@angular/material';
+
+/* Popups & Modals */
+import { MatBottomSheetModule } from '@angular/material';
 
 @NgModule({
   imports: [
     MatCardModule, MatListModule, MatIconModule, MatDividerModule,
     MatMenuModule, MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule, MatChipsModule,
     MatSnackBarModule, MatDialogModule, MatTooltipModule, MatInputModule, MatFormFieldModule, MatOptionModule,
-    MatAutocompleteModule, MatTableModule, MatPaginatorModule
+    MatAutocompleteModule, MatTableModule, MatPaginatorModule,
+
+    /* Popups & Modals */
+    MatBottomSheetModule
   ],
   exports: [
     MatCardModule, MatListModule, MatIconModule, MatDividerModule,
     MatMenuModule, MatButtonModule, MatCheckboxModule, MatProgressSpinnerModule, MatChipsModule,
     MatSnackBarModule, MatDialogModule, MatTooltipModule, MatInputModule, MatFormFieldModule, MatOptionModule,
-    MatAutocompleteModule, MatTableModule, MatPaginatorModule
+    MatAutocompleteModule, MatTableModule, MatPaginatorModule,
+
+    /* Popups & Modals */
+    MatBottomSheetModule
   ]
 })
 export class MaterialModule { }
