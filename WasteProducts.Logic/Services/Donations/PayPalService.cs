bf7db9be3a1779801533762f8ed1947f4569fed0
Andrew Palcheuski <timplayer@tut.by>
2018-09-28 15:00:53 +0300

    Add PayPalVerificationService class.

--- WasteProducts.Logic/Services/Donations/PayPalService.cs
+++ WasteProducts.Logic/Services/Donations/PayPalService.cs
@@ -29,42 +29,12 @@ namespace WasteProducts.Logic.Services.Donations
             var param = payPalRequest.BinaryRead(payPalRequest.ContentLength);
             var payPalRequestString = Encoding.ASCII.GetString(param);
 
-            HttpWebRequest verificationRequest = PrepareVerificationRequest(payPalRequestString);
 
-            // Send the request to PayPal and get the response
-            string verificationResponse = null;
-            using (var streamIn = new StreamReader(verificationRequest.GetResponse().GetResponseStream()))
-                verificationResponse = streamIn.ReadToEnd();
 
             ProcessVerificationResponse(verificationResponse, payPalRequestString);
         }
 
-        /// <summary>
-        /// Prepares a verification request.
-        /// </summary>
-        private HttpWebRequest PrepareVerificationRequest(string payPalRequestString)
-        {
-            const string PAYPAL_URL = "PayPalUrl";
-            const string VERIFICATION_PREFIX = "cmd=_notify-validate&";
-            const string POST = "POST";
-            const string CONTENT_TYPE = "application/x-www-form-urlencoded";
 
-            HttpWebRequest verificationRequest = (HttpWebRequest)WebRequest.Create(_appSettings[PAYPAL_URL]);
-
-            // Set values for the verification request
-            verificationRequest.Method = POST;
-            verificationRequest.ContentType = CONTENT_TYPE;
-
-            // Add cmd=_notify-validate to the payload
-            string verificationString = VERIFICATION_PREFIX + payPalRequestString;
-            verificationRequest.ContentLength = verificationString.Length;
-
-            // Attach payload to the verification request
-            using (var streamOut = new StreamWriter(verificationRequest.GetRequestStream(), Encoding.ASCII))
-                streamOut.Write(verificationString);
-
-            return verificationRequest;
-        }
 
         /// <summary>
         /// Processes a verification response.
