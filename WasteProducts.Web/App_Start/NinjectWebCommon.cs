5d3f64cebaccfe8b5a6f9e884dbf767c5f5caf37
Aleksander Shaduro <log.for.backend.m2@yandex.ru>
2018-09-28 19:44:53 +0300

    Added dynamic proxy module

--- WasteProducts.Web/App_Start/NinjectWebCommon.cs
+++ WasteProducts.Web/App_Start/NinjectWebCommon.cs
@@ -2,6 +2,7 @@
 using System.Web;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 using Ninject;
+using Ninject.Extensions.Interception;
 using Ninject.Web.Common;
 using Ninject.Web.Common.WebHost;
 
