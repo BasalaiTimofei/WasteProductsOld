b34fcec4ba82dd01dc894349cea2f7b250fcc0f3
SergeiTishkov <tishkovsergei92@gmail.com>
2018-09-29 17:25:57 +0300

    api URL changed to CI server

--- WasteProductsFront/src/environments/environment.prod.ts
+++ WasteProductsFront/src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ export const environment = {
   logLevel: 3,
 
   /* Api */
-  apiHostUrl: 'http://localhost:2189',
+  // apiHostUrl: 'http://localhost:2189',
+  apiHostUrl: 'http://waste-api.belpyro.net',
 
   /* Identity */
   iderntityHostUrl: 'https://localhost:44333',
