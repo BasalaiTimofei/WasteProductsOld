222653f12394576c90b419dd3322a0e21d0f3d78
Dr9vik <Dr9virane@gmail.com>
2018-09-29 13:48:19 +0300

    add model front

---
+++ WasteProductsFront/src/app/models/groups/groupBoard.ts
@@ -0,0 +1,11 @@
+import { GroupProduct } from 'src/app/models/groups/groupProduct';
+import { GroupComment } from 'src/app/models/groups/groupComment';
+
+export class GroupBoard{
+    Id:string;
+    Name:string;
+    CreatorId:string;
+    GroupId:string;
+    GroupProducts:Array<GroupProduct>;
+    GroupProductComments:Array<GroupComment>;
+}
