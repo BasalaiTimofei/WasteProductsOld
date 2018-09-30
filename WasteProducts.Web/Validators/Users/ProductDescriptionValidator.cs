cb208c48a71a491d9404e23dae589ad88aa47d7e
SergeiTishkov <tishkovsergei92@gmail.com>
2018-09-30 23:17:16 +0300

    hotfix of rating validation and input

--- WasteProducts.Web/Validators/Users/ProductDescriptionValidator.cs
+++ WasteProducts.Web/Validators/Users/ProductDescriptionValidator.cs
@@ -1,18 +1,20 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using WasteProducts.Web.Models.Users;
 
 namespace WasteProducts.Web.Validators.Users
 {
+    /// <summary>
+    /// Validates product descriptions.
+    /// </summary>
     public class ProductDescriptionValidator : AbstractValidator<ProductDescription>
     {
-        private const int _minProductRating = 1;
+        private const int _minProductRating = 0;
         private const int _maxProductRating = 5;
         private const int _maxProductDescriptionLength = 400;
 
+        /// <summary>
+        /// Constructor of this class.
+        /// </summary>
         public ProductDescriptionValidator()
         {
             RuleFor(x => x.Rating).NotNull().NotEmpty().GreaterThanOrEqualTo(_minProductRating)
