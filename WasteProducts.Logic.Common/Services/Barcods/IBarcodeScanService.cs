1e5ad5e739477f3c8e44399053d89c5fa6d543c2
Ilya Sauko <garsavko@mail.ru>
2018-10-04 21:06:33 +0300

    Added AForge nuget package, do black and white picture. Fixed scan method.

--- WasteProducts.Logic.Common/Services/Barcods/IBarcodeScanService.cs
+++ WasteProducts.Logic.Common/Services/Barcods/IBarcodeScanService.cs
@@ -12,10 +12,8 @@ namespace WasteProducts.Logic.Common.Services.Barcods
         /// Resize a image of barcode
         /// </summary>
         /// <param name="img"> image of barcode photo</param>
-        /// <param name="width"> width of barcode image result</param>
-        /// <param name="height"> height of barcode image result</param>
         /// <returns>Resized image</returns>
-        Stream Resize(Stream stream, int width, int height);
+        Stream Resize(Stream stream);
 
         /// <summary>
         /// get a numeric barcode from the photo
