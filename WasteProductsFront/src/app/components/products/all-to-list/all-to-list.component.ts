c6acc1c88f3fb10ddbbbd8af3f09a78472a3214d
MikhailShishlyannikov <mikhailshishlyannikov@gmail.com>
2018-10-05 15:13:22 +0300

    table with detail is working partially

--- WasteProductsFront/src/app/components/products/all-to-list/all-to-list.component.ts
+++ WasteProductsFront/src/app/components/products/all-to-list/all-to-list.component.ts
@@ -3,12 +3,20 @@ import { Product } from '../../../models/products/product';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 import { ProductService } from '../../../services/product/product.service';
 import { Router } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-all-to-list',
   templateUrl: './all-to-list.component.html',
   styleUrls: ['./all-to-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
   
   providers: [ProductService]
 })
