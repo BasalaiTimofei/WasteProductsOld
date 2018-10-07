e0f2345ed4e0cad66e5a68029a9772837bfa9289
MikhailShishlyannikov <mikhailshishlyannikov@gmail.com>
2018-10-07 11:24:19 +0300

    detail view in user field

--- WasteProductsFront/src/app/components/products/to-list/to-list.component.ts
+++ WasteProductsFront/src/app/components/products/to-list/to-list.component.ts
@@ -30,17 +30,18 @@ export class ToListComponent implements OnInit {
 
   products: Product[] = [];
   userProducts: UserProduct[] = [];
+  defualtImage: string = '../assets/img/tenor.gif';
+
   @Input() input = this.userProducts ;
   @Output() personListChange = new EventEmitter<Product[]>();
 
   data: UserProduct[] = this.userProducts;
   dataSource = new MatTableDataSource<UserProduct>();
-  displayedColumns: string[] = ['Name', 'AvgRating', 'Composition', 'IsHidden'];
+  displayedColumns: string[] = ['Id', 'Name', 'Rating', 'Description', 'IsHidden'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  datasourceSubject: TableDataSource<Product>;
 
   ngOnInit() {
     this.paginator.length = this.data.length;
@@ -48,6 +49,9 @@ export class ToListComponent implements OnInit {
 
 this.productService.getUserProducts().subscribe(
     res => {
+      for (let item of res) {
+        if (item.Product.PicturePath == "http://waste-api.belpyro.net/Content/favicon.png") item.Product.PicturePath = this.defualtImage; 
+      }
       this.userProducts = res;
       this.dataSource.data = res;
     },
--- WasteProductsFront/src/app/models/products/product-description.ts
+++ WasteProductsFront/src/app/models/products/product-description.ts
@@ -1,4 +1,7 @@
+import { Product } from 'src/app/models/products/product';
+
 export class ProductDescription {
   Rating: number;
   Description: string;
+  Product: Product;
 }
